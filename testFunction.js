edbf8145f8bdb0ad10173ab6e71c5ee623a90080
Anastasiia <ovnykca@gmail.com>
2018-09-20 21:11:17 -0400

    js function

---
+++ testFunction.js
@@ -0,0 +1,24 @@
+function add ( a, b) {
+ var z = a + b
+ console.log(sum)
+ return z
+}
+
+function subtend () {
+ var z = a - b
+ console.log(submit)
+ return z
+}
+
+function multiply() {
+ var z = a * b
+ console.log(multiply)
+ return z
+}
+
+function divide () {
+    var z = a / b
+    console.log(divide)
+    return z
+}
+
